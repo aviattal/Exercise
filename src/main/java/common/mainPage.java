64f98f3f5c574ba285980bc1842bc5c3f2a9b56d
aviat <avi.attal@gmail.com>
2020-05-24 22:36:16 +0300

    todo branch

--- src/main/java/common/mainPage.java
+++ src/main/java/common/mainPage.java
@@ -1,5 +1,7 @@
 package common;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
@@ -7,67 +9,92 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.ui.Select;
 
+import java.util.List;
+
 public class mainPage
 {
-/*    @FindBy(how = How.NAME, using ="firstname")
-    public WebElement txt_firstName;
+    public static WebDriver driver;
+    public static Actions action;
 
-    @FindBy(how = How.NAME, using ="lastname")
-    public WebElement txt_lastName;
-
-    @FindBy(how = How.NAME, using ="reg_email__")
-    public WebElement txt_email;
-
-    @FindBy(how = How.NAME, using ="reg_email_confirmation__")
-    public WebElement txt_email_confirm;
-
-    @FindBy(how = How.NAME, using ="reg_passwd__")
-    public WebElement txt_password;
-
-    @FindBy(how = How.NAME, using ="birthday_month")
-    public WebElement txt_birthday_month;
-
-    @FindBy(how = How.NAME, using ="birthday_day")
-    public WebElement txt_birthday_day;
-
-    @FindBy(how = How.NAME, using ="birthday_year")
-    public WebElement txt_birthday_year;
-
-    @FindBy(how = How.ID, using ="u_0_6")
-    public WebElement radio_female;
-
-    @FindBy(how = How.ID, using ="u_0_7")
-    public WebElement radio_male;
-
-    @FindBy(how = How.ID, using ="u_0_8")
-    public WebElement radio_custom;
-
-    @FindBy(how = How.NAME, using ="websubmit")
-    public WebElement btn_webSubmit;
-
-    @FindBy(how = How.ID, using ="reg_error_inner")
-    public WebElement error;
-
-    public void signUp( String fName, String lName, String email, String pass, String bday )
+    public void createItem(String name )
     {
-        txt_firstName.sendKeys(fName);
-        txt_lastName.sendKeys(lName);
-        txt_email.sendKeys(email);
-        txt_email_confirm.sendKeys(email);
-        txt_password.sendKeys(pass);
-        insertBirthDay(bday);
-        radio_male.click();
-        btn_webSubmit.click();
+        driver.findElement(By.className("new-todo")).sendKeys(name);
+        driver.findElement(By.className("new-todo")).sendKeys(Keys.RETURN);
+    }
+    public void filterAll()
+    {
+        driver.findElement(By.linkText("All")).click();
 
     }
-    public void insertBirthDay(String bday)
+    public List<WebElement> listItems()
     {
-        String [] values = bday.split("/");
-        Select Day = new Select(txt_birthday_day);
-        Select Month = new Select(txt_birthday_month);
-        Select Year = new Select(txt_birthday_year);
-        Month.selectByIndex(Integer.parseInt(values[1]));
-        Day.selectByVisibleText(values[0]);
-        Year.selectByVisibleText(values[2]);
-    }*/
+        return driver.findElements(By.xpath("/html/body/section/div/section/ul/li"));
+
+    }
+    public int getItemNumber()
+    {
+
+        return listItems().size();
+
+    }
+    public  void deleteItem (int index)
+    {
+
+        WebElement itemDeleteX= driver.findElement(By.tagName("button"));
+        action.moveToElement(listItems().get(index)).moveToElement(itemDeleteX).click().build().perform();
+
+
+        /*WebElement itemDeleteX = driver.findElement(By.tagName("button"));
+        List<WebElement> lisOfWebElements = listItems();
+        WebElement selected = lisOfWebElements.get(index);
+        action.moveToElement(selected).click().build().perform();*/
+    }
+
+    public void deleteAllItems()
+    {
+        int count = getItemNumber();
+        for (int i =0; i <count; i++ )
+        {
+            deleteItem(0);
+        }
+    }
+    public void toggleItem(int index)
+    {
+        listItems().get(index).findElement(By.className("toggle")).click();
+    }
+    public void filterCompleted()
+    {
+        driver.findElement(By.linkText("Completed")).click();
+    }
+    public void filterActive()
+    {
+        driver.findElement(By.linkText("Active")).click();
+    }
+    public void clearCompleted()
+    {
+        driver.findElement(By.className("clear-completed")).click();
+    }
+    //    public int getItemsLeft()
+//    {
+//      return driver.findElement(By.className("todo-count")
+//
+//    }
+    public String getItemName(int index)
+    {
+        return listItems().get(index).findElement(By.tagName("label")).getText();
+    }
+
+    public void reNameItem(int index, String newName)
+    {
+
+        int charCount = getItemName(index).length();
+        WebElement itemName = listItems().get(index).findElement(By.tagName("label"));
+        action.doubleClick(itemName).build().perform();
+        for (int i=0 ; i < charCount ; i++)
+        {
+            listItems().get(index).findElement(By.className("edit")).sendKeys(Keys.BACK_SPACE);
+        }
+        listItems().get(index).findElement(By.className("edit")).sendKeys(newName);
+        listItems().get(index).findElement(By.className("edit")).sendKeys(Keys.RETURN);
+    }
 }
--- src/test/java/business.java
+++ src/test/java/business.java
@@ -3,37 +3,87 @@ import common.mainPage;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.*;
 
 import java.util.concurrent.TimeUnit;
 
-public class business
-{
-    protected static WebDriver driver;
-  //  public static mainPage facebookLogin ;
+import static org.testng.Assert.assertEquals;
 
+public class business extends mainPage
+{
     @BeforeClass
     public void start()
     {
-
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+        action = new Actions(driver);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        String urlToOpen = commonMethodsClass.getData("urlToOpen");
-        driver.get(urlToOpen);
+        driver.get("http://todomvc.com/examples/react");
     }
     @AfterClass
-    public void close()
+    public void close() throws InterruptedException
     {
         driver.quit();
     }
+    @BeforeMethod
+    public void before()
+    {
+        createItem("AAA");
+        createItem("BBB");
+        createItem("CCC");
+    }
+    @AfterMethod
+    public void after()
+    {
+        filterAll();
+        deleteAllItems();
+    }
 
     @Test
-    public void test1_signUp()
+    public void test01_createItem()
     {
-  /*      facebookLogin.signUp(commonMethodsClass.getData("firstName"),commonMethodsClass.getData("lastName"), "bill.gates@microsoft.com", "12345","28/3/1956");
-        commonMethodsClass.verifyTextInElement(facebookLogin.error, "Your password must be at least 6 characters long. Please try another");*/
+        assertEquals(getItemNumber(),3);
+
+    }
+
+    @Test
+    public void test02_filterCompleted()
+    {
+        toggleItem(2);
+        filterCompleted();
+        assertEquals(getItemNumber(),1);
+    }
+    @Test
+    public void test03_filterActive()
+    {
+        toggleItem(2);
+        filterActive();
+        assertEquals(getItemNumber(),2);
+    }
+    @Test
+    public void test04_clearCompleted()
+    {
+        toggleItem(2);
+        clearCompleted();
+        assertEquals(getItemNumber(),2);
+    }
+    @Test
+    public void test05_itemLeft()
+    {
+    }
+    @Test
+    public void test06_renameItem()
+    {
+        reNameItem(2, "DDD");
+        assertEquals(getItemName(2), "DDD");
+    }
+    @Test
+    public void test07_deleteItem()
+    {
+        deleteItem(0);
+        assertEquals(getItemNumber(),2);
     }
 
 }
